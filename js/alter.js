ffe5fde3e86ef2c9c6de0469aae4232c7b41adf5
McKay H <admin@McKay.local>
2022-03-17 08:13:52 -0600

    adding stuff

--- js/alter.js
+++ js/alter.js
@@ -4,3 +4,15 @@ function setInfo(item){
     const page = document.getElementById('page');
     page.style.backgroundImage = `url('${item}')`;
 }
+
+{/* <td class="mdl-data-table__cell--non-numeric"><form action="#">
+  <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
+    <label class="mdl-button mdl-js-button mdl-button--icon" for="sample6">
+      <span id="something">0</span>
+    </label>
+    <div class="mdl-textfield__expandable-holder">
+      <input class="mdl-textfield__input" type="text" id="sample6">
+      <label class="mdl-textfield__label" for="sample-expandable">Expandable Input</label>
+    </div>
+  </div>
+</form></td> */}
