b463d910b14860d276564d4732637b6abfa72282
McKay H <admin@McKay.local>
2022-03-17 10:15:27 -0600

    switched to bootstrap

--- js/main.js
+++ js/main.js
@@ -1,32 +1,27 @@
-const golfAPI = fetchGolf();
+const golfAPI = CoursApi();
 
-golfAPI.then(displayCard);
+// golfAPI.then(displayCard);
 const golfHole = fetchHole();
 
 
-function fetchGolf(){
-    return fetch('https://golf-courses-api.herokuapp.com/courses/').then(
-        function (response) {
-            return response.json();
-        }
-        );
-    }
-function fetchHole(){
-    return fetch('https://golf-courses-api.herokuapp.com/courses/11819').then(
-        function (response) {
-            return response.json();
-        }
-        );
-    }
-function displayCard(card){
-    //console.log(card);
-    let htm = '';
-    card.courses.forEach(item => {
-        htm += `<span class="mdl-navigation__link clicky" onclick="setInfo('${item.image}')">${item.name}</span>`;
-    });
-    document.getElementById('navLinks').innerHTML = htm;
-    document.getElementById('navLinksSide').innerHTML = htm;
+async function CoursApi(){
+    const getting = await fetch('https://golf-courses-api.herokuapp.com/courses/')
+    return getting.json()
+}
+async function fetchHole(){
+    const getting = await fetch('https://golf-courses-api.herokuapp.com/courses/11819')
+    return getting.json()
+        
+}
+// function displayCard(card){
+//     console.log(card);
+//     let htm = '';
+//     card.courses.forEach(item => {
+//         htm += `<span class="clicky" onclick="setInfo('${item.image}')">${item.name}</span>`;
+//     });
+//     document.getElementById('navLinks').innerHTML = htm;
+//     document.getElementById('navLinksSide').innerHTML = htm;
     
     // console.log(htm);
 
-}
+// }
